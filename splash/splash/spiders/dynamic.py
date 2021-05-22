89b7614664c0cf2a94304bcff45f9de20124e9eb
Geo Thomas <geothomas@cet.ac.in>
2021-05-22 08:55:49 +0530

    day2

--- splash/splash/spiders/dynamic.py
+++ splash/splash/spiders/dynamic.py
@@ -2,7 +2,7 @@ import scrapy
 from scrapy_splash import SplashRequest
 main_list=[]
 key=[
-    'hotspot+shield'
+    'likes+for+instagram'
 ]
 class DynamicSpider(scrapy.Spider):
     name = 'dynamic'
