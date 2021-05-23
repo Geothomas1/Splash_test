73432912da2b3d03256c5ee2676102d93362ed9a
Geo Thomas <geothomas@cet.ac.in>
2021-05-23 11:41:33 +0530

    Day3

--- splash/splash/spiders/dynamic.py
+++ splash/splash/spiders/dynamic.py
@@ -2,7 +2,7 @@ import scrapy
 from scrapy_splash import SplashRequest
 main_list=[]
 key=[
-    'Match+3'
+    'Massively+multiplayer+online+role+playing'
 ]
 class DynamicSpider(scrapy.Spider):
     name = 'dynamic'
