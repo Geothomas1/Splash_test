28146944a2e3f4a2b2e8a09702b6c4fb3cc229da
Geo Thomas <geothomas@cet.ac.in>
2021-05-23 14:44:26 +0530

    Day3

--- splash/splash/spiders/dynamic.py
+++ splash/splash/spiders/dynamic.py
@@ -2,7 +2,7 @@ import scrapy
 from scrapy_splash import SplashRequest
 main_list=[]
 key=[
-    'Canada'
+    'Central+African+Republic'
 ]
 class DynamicSpider(scrapy.Spider):
     name = 'dynamic'
