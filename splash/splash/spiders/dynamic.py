30f70cd146e761bafcfec2fba091172694ac3057
Geo Thomas <geothomas@cet.ac.in>
2021-05-23 20:37:58 +0530

    day 3

--- splash/splash/spiders/dynamic.py
+++ splash/splash/spiders/dynamic.py
@@ -2,7 +2,7 @@ import scrapy
 from scrapy_splash import SplashRequest
 main_list=[]
 key=[
-    'Andhra+Pradesh'
+    '	Arunachal+Pradesh'
 ]
 class DynamicSpider(scrapy.Spider):
     name = 'dynamic'
