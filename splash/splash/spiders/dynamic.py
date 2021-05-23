eec95889267d38ef37411b64725e448897b8cca4
Geo Thomas <geothomas@cet.ac.in>
2021-05-23 11:38:21 +0530

    Day3

--- splash/splash/spiders/dynamic.py
+++ splash/splash/spiders/dynamic.py
@@ -2,7 +2,7 @@ import scrapy
 from scrapy_splash import SplashRequest
 main_list=[]
 key=[
-    'Multiplayer+online+battle+arena'
+    'Motorcycle+racing'
 ]
 class DynamicSpider(scrapy.Spider):
     name = 'dynamic'
