c5c2d031215fe610794deadf702d1bbbd87f296d
Geo Thomas <geothomas@cet.ac.in>
2021-05-24 12:33:59 +0530

    Day4

--- splash/splash/spiders/dynamic.py
+++ splash/splash/spiders/dynamic.py
@@ -2,7 +2,12 @@ import scrapy
 from scrapy_splash import SplashRequest
 main_list=[]
 key=[
-    'Backbone'
+    'Backer',
+    'Backup',
+    'Bada+bing+bada+boom'
+    'Badass',
+    'Badassery',
+    'Balance'
 ]
 class DynamicSpider(scrapy.Spider):
     name = 'dynamic'
