5081c166bc5c34993c326c78774624195608b5f3
Geo Thomas <geothomas@cet.ac.in>
2021-06-14 14:43:43 +0530

    change

---
+++ Dataset_Api/columname.py
@@ -0,0 +1,1 @@
+App Name	App Id	Category	Rating	Rating Count	Installs	Minimum Installs	Free	Price	Currency	Size	Minimum Android	Developer Id	Developer Website	Developer Email	Released	Last update	Privacy Policy	Content Rating	Ad Supported	In app purchases	Editor Choice	Summary	Reviews	Android version Text	Developer	Developer Address	Developer Internal ID	Version
